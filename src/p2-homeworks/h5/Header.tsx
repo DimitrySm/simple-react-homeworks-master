dc2bc4a371f32487936a21715ac66dd2c79c9e65
Dmitriy Smaliak <you@example.com>
2021-01-05 17:38:41 +0300

    commit

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
-
-        </div>
-    );
+  return (
+    <div>
+      <nav>
+        <NavLink to="/hw1">H1</NavLink>
+        <NavLink to="/hw2">H2</NavLink>
+        <NavLink to="/hw3">H3</NavLink>
+        <NavLink to="/hw4">H4</NavLink>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,29 +1,36 @@
 import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import HW1 from "../h1/HW1";
+import HW2 from "../h2/HW2";
+import HW3 from "../h3/HW3";
+import HW4 from "../h4/HW4";
+import Error404 from "./pages/Error404";
+import PreJunior from "./pages/PreJunior";
 
 export const PATH = {
-    PRE_JUNIOR: "/pre-junior",
-    // add paths
-}
+  PRE_JUNIOR: "/pre-junior",
+  // add paths
+};
 
 function Routes() {
-    return (
-        <div>
-            {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+  return (
+    <div>
+      {/* Switch выбирает первый подходящий роут */}
+      <Switch>
+        {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
+        {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
+        <Route path={"/"} exact render={() => <Redirect to="/pre-junior" />} />
+        <Route path="/pre-junior" render={() => <PreJunior />} />
+        <Route path="/hw1" render={() => <HW1 />} />
+        <Route path="/hw2" render={() => <HW2 />} />
+        <Route path="/hw3" render={() => <HW3 />} />
+        <Route path="/hw4" render={() => <HW4 />} />
 
-            {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
-            {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
-            {/*<Route path={"/"} exact render={() => <Redirect to={PRE_JUNIOR}/>}/>*/}
-
-            {/*<Route path={PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
-                // add routes
-
-            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
-
-            {/*</Switch>*/}
-        </div>
-    );
+        {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+        <Route render={() => <Error404 />} />
+      </Switch>
+    </div>
+  );
 }
 
 export default Routes;
