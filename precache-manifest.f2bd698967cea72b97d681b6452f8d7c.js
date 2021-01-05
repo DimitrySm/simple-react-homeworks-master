415b84b8f526425b49047b5189a133a202a918fa
Dmitriy Smaliak <you@example.com>
2021-01-05 17:10:36 +0300

    Updates

---
+++ precache-manifest.f2bd698967cea72b97d681b6452f8d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad09be2e039e5e6da64f7ab0041eb77",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "4fce4dc766bca02992c5",
+    "url": "/simple-react-homeworks-master/static/css/main.e08dd297.chunk.css"
+  },
+  {
+    "revision": "29318307b88b169d6147",
+    "url": "/simple-react-homeworks-master/static/js/2.8fb1696b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-homeworks-master/static/js/2.8fb1696b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fce4dc766bca02992c5",
+    "url": "/simple-react-homeworks-master/static/js/main.60fa3ef1.chunk.js"
+  },
+  {
+    "revision": "33f020c42a00208bc7a5",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.074d676d.js"
+  }
+]);
